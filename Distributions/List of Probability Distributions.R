c892cc6f64cbf21b70ee07a8b8735809b70ea512
Patrick <spohrpatrick@gmail.com>
2024-04-29 17:24:44 +0200

    create list of all distributions in R

---
+++ Distributions/List of Probability Distributions.R
@@ -0,0 +1,60 @@
+#### List of Probability Distributions in R ####
+## d (density), p (cumulative probability), q (quantile), r (random generator)
+
+
+# binomial (including Bernoulli) distribution
+?dbinom
+?pbinom
+?qbinom
+?rbinom
+
+# geometric distribution
+?dgeom
+?pgeom
+?qgeom
+?rgeom
+
+# normal distribution
+?dnorm
+
+# log-normal distribution
+?dlnorm
+
+# exponential distribution
+?dexp
+
+# Poisson distribution
+?dpois
+
+# Student's t distribution
+?dt
+
+# uniform distribution
+?dunif
+
+# hypergeometric distribution
+?dhyper
+
+# gamma distribution
+?dgamma
+
+# chi-squared distribution
+?dchisq
+
+# negative binomial distribution
+?dnbinom
+
+# F distribution
+?df
+
+# multinomial distribution
+?dmultinom
+
+# Weibull distribution
+?dweibull
+
+# Cauchy distribution
+?dcauchy
+
+# beta distribution
+?dbeta.
