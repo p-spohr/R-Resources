52af0709ec4327a05ca13166ab9c2d72358568a1
Patrick <spohrpatrick@gmail.com>
2024-07-10 18:51:35 +0200

    filter and covariance practice

---
+++ Time Series/Filters.R
@@ -0,0 +1,55 @@
+# Difference filter
+
+wn <- rnorm(10000)
+mean(wn)
+var(wn)
+
+
+# autocovariance
+autocov <- function(V, tau = 0) {
+  tau <- abs(tau)
+  V_1 <- V[1:(length(V)-tau)]
+  V_2 <- V[(1+tau):length(V)]
+  return(cov(V_1, V_2))
+}
+
+autocov(wn)
+
+
+for (j in -2:2) {
+  print(sprintf("tau is %i, autocov is %.4f", j, autocov(wn, j)))
+}
+# !!!!! paste, paste0, and sprintf need to be printed using print when in a loop
+
+?diff
+
+wn_diff_1 <- diff(wn, 1)
+mean(wn_diff_1)
+var(wn_diff_1)
+
+
+for (j in -2:2) {
+  print(sprintf("tau is %i, autocov is %.4f", j, autocov(wn_diff_1, j)))
+}
+
+ts.plot(wn_diff_1)
+
+wn_diff_2 <- diff(wn, 2)
+mean(wn_diff_2)
+var(wn_diff_2)
+
+
+for (j in -2:2) {
+  print(sprintf("tau is %i, autocov is %.4f", j, autocov(wn_diff_2, j)))
+}
+
+ts.plot(wn_diff_2)
+ts.plot(cumsum(wn))
+ts.plot(cumsum(wn_diff_1))
+
+twn <- rt(5000, 10)
+ts.plot(cumsum(twn))
+ts.plot(diff(twn,1))
+mean(twn)
+var(twn)
+mean(diff(twn, 1))
--- Time Series/Unbiased Symmetrical Moving Average.R
+++ Time Series/Unbiased Symmetrical Moving Average.R
@@ -35,3 +35,47 @@ cov(WN[1:(length(WN)-1)], WN[(1+1):length(WN)]) # covariance is 0 when index is 
 
 cov(RW, RW) # covariance of random walk is not 1 because they are not independent
 
+
+wn_1 <- rnorm(500)
+wn_ma_3 <- ma(wn_1, order = 3)
+wn_ma_3 <- wn_ma_3[!is.na(wn_ma_3)]
+
+wn_ma_5 <- ma(wn_1, order = 5)
+wn_ma_5 <- wn_ma_5[!is.na(wn_ma_5)]
+
+wn_ma_6 <- ma(wn_1, order = 6)
+wn_ma_6 <- wn_ma_6[!is.na(wn_ma_6)]
+
+wn_ma_10 <- ma(wn_1, order = 10)
+wn_ma_10 <- wn_ma_10[!is.na(wn_ma_10)]
+
+U <- 1:15
+ma(U, 10, centre = TRUE)
+?ma
+
+# expected value of MA from white noise is 0
+mean(wn_ma_3)
+mean(wn_ma_10)
+
+
+# autocovariance
+autocov <- function(V, tau = 0) {
+  V_1 <- V[1:(length(V)-tau)]
+  V_2 <- V[(1+tau):length(V)]
+  print(cov(V_1, V_2))
+}
+
+wn_2 <- rnorm(10000)
+autocov(wn_2)
+autocov(wn_2, 1)
+
+
+wn_2_ma_3 <- ma(wn_2, 3)
+wn_2_ma_3 <- wn_2_ma_3[!is.na(wn_2_ma_3)]
+var(wn_2)
+var(wn_2_ma_3) # ma is 1/3 the variance of wn
+autocov(wn_2_ma_3) # autocov with tau 0 is 1/3 the variance of wn
+autocov(wn_2_ma_3, 1) # autocov with tau 1 is 2/9 the variance of wn
+autocov(wn_2_ma_3, 2) # autocov with tau 1 is 1/9 the variance of wn
+var(wn_2) * 1/9
+autocov(wn_2_ma_3, 3) # autocov with tau 1 is 1/9 the variance of wn
